96458a0b1bd805ba7953737f995e1fb7a50ca5d5
muthioo-faith <muthioofaith@gmail.com>
2022-10-06 23:25:13 +0300

    upeowebsite

---
+++ src/upeo.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import "./upeo.css"
+import pic from "./lad.png";
+import logo from "./upeologo.png";
+import ussd from "./ussd.png";
+
+const Navbar=()=>{
+  return(
+<body>
+    <div>
+      <nav>
+        <ul>
+        <div class="topnav">
+{/* <div className='logo'>
+    <img src={logo} alt='./upeologo.png'/>
+
+  </div> */}
+
+  <a href="#home">Home</a>
+  <a href="#about us">About us</a>
+  <a href="#technology">Technology</a>
+  <a href="#contact">Contact</a>
+  <div className='logo'>
+    <img src={logo} alt='./upeologo.png' width={140}/>
+
+  </div>
+
+  <div className='image'>
+    <img src={pic} />
+
+  </div>
+</div>
+<h2>About Us</h2>
+<p>We discovered that their is a problem, 
+  where  pregnant<br></br> teenage girls who drop out 
+  of school are unable to continue <br></br> their education
+   while on maternity. We also found out that <br></br>  when 
+   they return to school they have forgotten what they <br></br> 
+   learned .We decided to come up with Quicksoma platform<br></br> 
+    which is an interactive learning channel that provides<br></br> 
+     revision questions for them to continue learning while<br></br>  
+     on Maternity.
+</p>
+<h3>How it works</h3>
+<div className='ussd'>
+    <img src={ussd} alt='./ussd.png' width={450}/>
+
+  </div>
+<h6>QuickSoma is an offline  learning platform that uses a USSD 
+  code with<br></br> the goal of assisting  pregnant teenage students
+  while on maternity.They<br></br> will use feature phones to help them 
+  access and tackle questions that <br></br>will help them revise and  stay 
+  on top of their academics in a variety of subjects, 
+  <br></br>such as mathematics. </h6>
+
+  <h5>Team</h5>
+
+
+</ul>
+
+</nav>
+
+</div>
+</body>
+  )
+  
+}
+export default Navbar;
